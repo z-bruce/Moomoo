84838a15f5eaada7bd23f91d6342d549146866ec
Jay970719 <happyaustin0@gmail.com>
2021-08-02 12:44:55 -0400

    updated buttons

--- js/meat-product.js
+++ js/meat-product.js
@@ -1,1 +1,11 @@
 //Jay's javascript
+
+function showdescription(){
+   d= document.getElementById("description").style;
+   
+   if(d.display == "none")
+        d.display = "block";
+
+   else d.display= "none";
+
+}
