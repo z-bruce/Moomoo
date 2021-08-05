f4a4bb3ee916be98874bf396a5a58c7369195524
z-bruce <zackrbruce@gmail.com>
2021-08-05 16:54:10 -0400

    added sessionStorage functionality and modified added to cart alert

--- js/beverage-products.js
+++ js/beverage-products.js
@@ -4,6 +4,22 @@ function moreDescription() {
   desc.display == "none" ? desc.display = "block" : desc.display = "none";
 }
 
-function addedToCartAlert(productName) {
-  alert("Added " + productName + " to cart.");
+function addedToCartAlert() {
+  var productName = document.getElementById("productName").innerHTML;
+  var productQty = document.getElementById("quantity").value;
+  alert("Added " + productQty + "x " + productName + " to cart.");
+}
+
+function loadQty() {
+  var productName = document.getElementById("productName").innerHTML;
+  var qty = document.getElementById("quantity");
+  if (sessionStorage.getItem(productName)) {
+    qty.value = sessionStorage.getItem(productName);
+  }
+}
+
+function saveQty() {
+  var productName = document.getElementById("productName").innerHTML;
+  var qty = document.getElementById("quantity");
+  sessionStorage.setItem(productName, qty.value);
 }
