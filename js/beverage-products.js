4a427ad1fd6f523f2f0b0d77a46875f905255967
z-bruce <zackrbruce@gmail.com>
2021-08-09 00:35:13 -0400

    swapped added to cart alert for bootstrap popup

--- js/beverage-products.js
+++ js/beverage-products.js
@@ -1,25 +1,10 @@
+//shows description of product
 function moreDescription() {
   desc = document.getElementById("more-description").style;
   desc.display == "none" ? desc.display = "block" : desc.display = "none";
 }
 
-function addedToCartAlert() {
-  var productName = document.getElementById("productName").innerHTML;
-  var productQty = document.getElementById("quantity").value;
-
-  alert("Added " + productQty + "x " + productName + " to cart.");
-}
-
-function loadQty() {
-  var productName = document.getElementById("productName").innerHTML;
-  var qty = document.getElementById("quantity");
-  
-  if (sessionStorage.getItem(productName)) {
-    qty.value = sessionStorage.getItem(productName);
-  }
-  calculateSubtotal();
-}
-
+//saves quantity of product in Session Storage
 function saveQty() {
   var productName = document.getElementById("productName").innerHTML;
   var qty = document.getElementById("quantity");
@@ -27,10 +12,25 @@ function saveQty() {
   sessionStorage.setItem(productName, qty.value);
 }
 
+//loads quantity of product from Session Storage
+function loadQty() {
+  var productName = document.getElementById("productName").innerHTML;
+  var qty = document.getElementById("quantity");
+
+  if (sessionStorage.getItem(productName)) {
+    qty.value = sessionStorage.getItem(productName);
+  }
+  calculateSubtotal();
+}
+
+//calculates subtotal of product based on quantity selected, and updates quantity for popup
 function calculateSubtotal() {
+  var popupQty = document.getElementById("popup-quantity");
   var quantity = document.getElementById("quantity").value;
   var subtotal = document.getElementById("subtotal");
   var unitPrice = parseFloat(document.getElementById("unit-price").innerHTML);
+
+  popupQty.innerHTML = quantity;
 
   subtotal.innerHTML = (quantity * unitPrice).toFixed(2);
 }
